04b050664f2a7fe9a21101e0289820909e5fe473
Noah Rondeau <ntrondeau@gmail.com>
2018-11-01 01:43:07 -0700

    Remove interrupt for millisecond timer

--- timer.c
+++ timer.c
@@ -10,40 +10,6 @@
 #define LED_RIGHT_END (0b00000011)
 #define LED_LEFT_END (0b11000000)
 
-
-int main(void)
-{
-
-	unsigned char led_out = LED_RIGHT_END;
-	unsigned int led_dir = LED_DIR_LEFT;
-
-	// Initialize timer
-	TIMER_Init();
-	// initialize port D to output
-	DDRC = 0xff;
-
-	while (1)
-	{
-		PORTC = led_out;
-
-		// switch direction if necessary
-		if (led_out == LED_LEFT_END)
-			led_dir = LED_DIR_RIGHT;
-		
-		if (led_out == LED_RIGHT_END)
-			led_dir = LED_DIR_LEFT;
-
-		//bit shift output
-		if (led_dir == LED_DIR_LEFT)
-			led_out = led_out << 1;
-		else // led_dir == LED_DIR_RIGHT
-			led_out = led_out >> 1;
-
-		TIMER_CountMs(200);
-	}
-
-}
-
 void TIMER_Init(void)
 {
 	TCCR1B |= _BV(CS10);
@@ -59,8 +25,10 @@ void TIMER_CountMs(int ms)
 	OCR1A = 0x03e8;
 	// Reset the timer
 	TCNT1 = 0x0000;
+
+	//DONT DO THIS!!! we dont want interrupt
 	// Enable the output compare interrupt enable
-	TIMSK1 = TIMSK1 | 0b00000010;
+	//TIMSK1 = TIMSK1 | 0b00000010;
 	// clear interrupt flag and begin counting
 	TIFR1 |= _BV(OCF1A);
 
