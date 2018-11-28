0f0d8413732dc09bc79a16f22f23f2b7e97dec4f
Noah Rondeau <ntrondeau@gmail.com>
2018-11-27 18:45:26 -0800

    Revert "New filter impl, need to test"
    This reverts commit 1cca3bec173de8ef0ea17ad8abf813d7880300e3.

--- Mech458Project/main.c
+++ Mech458Project/main.c
@@ -200,7 +200,10 @@ void Initialize()
 	TRAY_Init(&tray);
 	TRAY_Home(&tray);
 	
-	FILTER_Reset(&adcFilter,1023); // initialize to most likely first value;
+	// initialize filter
+	float num[] = FILTER_NUMER_COEFFS;
+	float den[] = FILTER_DENOM_COEFFS;
+	FILTER_Init(&adcFilter, num, den, 1023); // initialize to most likely first value;
 	// in the future, could do an ADC run and set to the average value of the background found
 	// perhaps in an ADC_Calibrate function
 	
@@ -349,7 +352,7 @@ ISR(INT2_vect)
 		Stage2.adcContinueConversions = true;
 		Stage2.sampleCount = 0; // reset sample counter
 		Stage2.minReflectivity = LARGEST_UINT16_T; // reset to default reflectivity
-		FILTER_Reset(&adcFilter, 1023);
+		FILTER_ResetWithPadding(&adcFilter, 1023);
 		ADC_StartConversion(&adc);
 	}
 	else // just saw rising edge
