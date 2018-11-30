ab40bf016cc7f7365627606a59f36fce3ed442b7
Noah Rondeau <ntrondeau@gmail.com>
2018-11-30 12:10:32 -0800

    Update UART functions with comments

--- Mech458Project/uart.c
+++ Mech458Project/uart.c
@@ -15,15 +15,29 @@ void uint16ToString(uint16_t byte, char* out)
 	out[15-i] = (((byte >> i) & 0x01) == 1) ? '1' : '0';
 }
 
-void UART_Init(uint32_t baudrate)
+void UART_Init(uint32_t baud)
 {
 	//empty
+	/*
+	UBRRHn = (unsigned char)(baud>>8);
+	UBRRLn = (unsigned char)baud;
+	// Enable receiver and transmitter 
+	UCSRnB = (1<<RXENn)|(1<<TXENn);
+	// Set frame format: 8data, 2stop bit
+	UCSRnC = (1<<USBSn)|(3<<UCSZn0);*/
 }
 
 void UART_SendChar(char c)
 {
 	// empty
 	LED_Set((uint8_t)c);
+	/*
+	// from hardware user manual
+	//Wait for empty transmit buffer
+	while ( !( UCSRnA & (1<<UDREn)) );
+	//Put data into buffer, sends the data
+	UDRn = c;
+	*/
 }
 void UART_SendString(const char* s, uint16_t len)
 {
--- Mech458Project/uart.h
+++ Mech458Project/uart.h
@@ -12,9 +12,9 @@
 #include "config.h"
 
 void uint16ToString(uint16_t byte, char* out);
-void UART_Init(uint32_t baudrate);
+void UART_Init(uint32_t baud);
 void UART_SendChar(char c);
-void UART_SendString(const char* s, uint16_t len);
+void UART_SendString( const char* s, uint16_t len);
 
 
 #endif /* UART_H_ */
