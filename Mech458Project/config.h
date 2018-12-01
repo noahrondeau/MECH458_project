abf0aa2bb9ae623af246f83a43095b6ad97a474d
Noah Rondeau <ntrondeau@gmail.com>
2018-11-30 22:39:29 -0800

    New max speed
    still 20 ms for max delay, but 6 ms for min delay

--- Mech458Project/config.h
+++ Mech458Project/config.h
@@ -76,7 +76,7 @@ typedef volatile uint8_t  GPIOMask;
 #define STEPPER_DDR				(DDRA_REG)
 #define STEPPER_PORT			(PORTA_REG)
 #define STEPPER_DELAY_MAX		(20)
-#define STEPPER_DELAY_MIN		(8)
+#define STEPPER_DELAY_MIN		(6)
 #define STEPPER_ACCEL_RAMP		(STEPPER_DELAY_MAX - STEPPER_DELAY_MIN)
 #define STEPPER_STARTUP_DELAY		(STEPPER_DELAY_MAX + 1) // for the math to work out
 
