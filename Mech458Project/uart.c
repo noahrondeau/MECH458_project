5d6e74a47cd7c2636c5e0310f81acfc9e3a5fb52
Noah Rondeau <ntrondeau@gmail.com>
2018-12-06 00:13:20 -0800

    add uart.h and uart.c to master

---
+++ Mech458Project/uart.c
@@ -0,0 +1,44 @@
+/*
+ * uart.c
+ *
+ * Created: 2018-11-29 10:00:43 PM
+ *  Author: ntron
+ */ 
+
+#include "uart.h"
+#include "LedBank.h"
+
+void uint16ToString(uint16_t byte, char* out)
+{
+	// store the character in the inverted order, since we will be sending them as strings
+	for(int i = 0; i < 16; i++)
+	out[15-i] = (((byte >> i) & 0x01) == 1) ? '1' : '0';
+}
+
+void UART_Init()
+{
+	// set baud to 9600 -> UBRRn = (sysclock_f / 16*baud) - 1 = 51 = 0b00110011
+	//empty
+	UBRR1 = (uint8_t)51;
+	// Enable transmitter
+	UCSR1B = (1<<TXEN1);
+	// Set frame format: 8data, 1stop bit, no parity (8N1)
+	UCSR1C = (1<<UCSZ10)| (1 << UCSZ11);
+}
+
+void UART_SendChar(char c)
+{
+	// empty
+	LED_Set((uint8_t)c);
+	while ( !( UCSR1A & (1<<UDRE1)) );
+	UDR1 = c;
+}
+void UART_SendString(const char* s)
+{
+	unsigned int counter = 0;
+	while( s[counter] != '\0')
+	{
+		UART_SendChar(s[counter]);
+		counter++;
+	}
+}
